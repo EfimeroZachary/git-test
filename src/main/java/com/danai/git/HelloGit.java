ec3d2d1c4bf7e505237e40346d2295a47ae7701b
Zachary <sleep.xenogenesis@gmail.com>
2024-04-20 02:17:55 +0800

    hot-fix commit1

--- src/main/java/com/danai/git/HelloGit.java
+++ src/main/java/com/danai/git/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("hehehe");
+        System.out.println("xixixi");
+        System.out.println("xixixi 12 18 36");
     }
 }
